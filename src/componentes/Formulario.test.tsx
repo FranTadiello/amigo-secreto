c60596cb54e603ce5b06458361c187bf119a9e9a
Franciele Tadiello <franciele.p.t@gmail.com>
2023-07-19 11:30:34 -0300

    Buscando elementos na tela

---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+//usando Jest e react
+test('quando o input esta vazio, novos participantes não podem ser adicionados', () => {
+    render(<Formulario />)
+    //encontrar no DOM o input (buscando na tela o texto)
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    //encontrar botão (pela responsabilidade do elemento - submeter)
+    const botao = screen.getByRole('button')
+    //garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+    //garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+})
