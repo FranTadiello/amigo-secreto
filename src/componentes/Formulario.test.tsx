1416b2f0c5f40773d2aafdd3cfb9b6bcce49542e
Franciele Tadiello <franciele.p.t@gmail.com>
2023-07-20 14:16:04 -0300

    Criando teste para adição de participante

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
 
@@ -13,4 +13,24 @@ test('quando o input esta vazio, novos participantes não podem ser adicionados'
     expect(input).toBeInTheDocument()
     //garantir que o botão esteja desabilitado
     expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(<Formulario />)
+    //encontrar no DOM o input (buscando na tela o texto)
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    //encontrar botão (pela responsabilidade do elemento - submeter)
+    const botao = screen.getByRole('button')
+    //Inserir valor no input (usuário digitando)
+    fireEvent.change(input,{
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
+    //Clicar no botão de submeter
+    fireEvent.click(botao)
+    //Garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+    //Garantir que o input não tenha um valor (limpar após submeter)
+    expect(input).toHaveValue('')
 })
