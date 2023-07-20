44f3a065bda1ff3e632c3e3cd31bc55dedbd1501
Franciele Tadiello <franciele.p.t@gmail.com>
2023-07-20 14:38:30 -0300

    useRef e o foco no input

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from 'recoil';
 
 //usando Jest e react
 test('quando o input esta vazio, novos participantes não podem ser adicionados', () => {
@@ -16,7 +17,7 @@ test('quando o input esta vazio, novos participantes não podem ser adicionados'
 })
 
 test('adicionar um participante caso exista um nome preenchido', () => {
-    render(<Formulario />)
+    render(<RecoilRoot><Formulario /></RecoilRoot>)
     //encontrar no DOM o input (buscando na tela o texto)
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
     //encontrar botão (pela responsabilidade do elemento - submeter)
